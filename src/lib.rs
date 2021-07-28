88e06ec3fa4c671092a7083f473536bc107ae8ed
Cabral <17774851+cabralski@users.noreply.github.com>
2021-07-27 22:37:15 -0300

    fixed a pretty concerning bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-//! `intname` is a light and tiny crate for generating integer names based on any integer type ranging from `u8` to `u128` and `i8` to `i128`.
-//! The maximum value supported for integers is `u128`.
+//! `intname` is a light and tiny crate for generating integer names based on any integer type ranging from `u8` to `u64` and `i8` to `i128`.
+//! The maximum value supported for integers is `[i128::MIN + 1, i128::MAX]`.
 //!
 //! ```rust
 //! use intname::integer_name;
-//! assert_eq!(&integer_name(42), "forty-two")
+//! assert_eq!(&integer_name(42), "forty-two");
+//! ```
+//!
+//! Works seamless with `signed` integers.
+//! ```rust
+//! use intname::integer_name;
+//! assert_eq!(&integer_name(i32::MAX), "two billion, one hundred forty-seven million, four hundred eighty-three thousand, six hundred forty-seven");
 //! ```
 //!
 //! Huge `signed` or `unsigned` integers can be parsed in nanoseconds.
@@ -73,7 +79,9 @@ pub fn integer_name<T: PrimInt + ToPrimitive>(int: T) -> String {
         name.push_str("negative ")
     }
     
-    let mut int: u128 = int.to_u128().unwrap();
+    let int: i128 = int.to_i128().unwrap();
+    let mut int: i128 = int.abs();
+
     let mut hundreds: Vec<u16> = Vec::new();
 
     while int > 0 {
@@ -118,8 +126,8 @@ mod tests {
 
     #[test]
     fn big_numbers() {
+        assert_eq!(&integer_name(-170141183460469231731687303715884105727i128), "negative one hundred seventy undecillion, one hundred forty-one decillion, one hundred eighty-three nonillion, four hundred sixty octillion, four hundred sixty-nine septillion, two hundred thirty-one sextillion, seven hundred thirty-one quintillion, six hundred eighty-seven quadrillion, three hundred three trillion, seven hundred fifteen billion, eight hundred eighty-four million, one hundred five thousand, seven hundred twenty-seven");
         assert_eq!(&integer_name(170141183460469231731687303715884105727i128), "one hundred seventy undecillion, one hundred forty-one decillion, one hundred eighty-three nonillion, four hundred sixty octillion, four hundred sixty-nine septillion, two hundred thirty-one sextillion, seven hundred thirty-one quintillion, six hundred eighty-seven quadrillion, three hundred three trillion, seven hundred fifteen billion, eight hundred eighty-four million, one hundred five thousand, seven hundred twenty-seven");
-        assert_eq!(&integer_name(340282366920938463463374607431768211455u128), "three hundred forty undecillion, two hundred eighty-two decillion, three hundred sixty-six nonillion, nine hundred twenty octillion, nine hundred thirty-eight septillion, four hundred sixty-three sextillion, four hundred sixty-three quintillion, three hundred seventy-four quadrillion, six hundred seven trillion, four hundred thirty-one billion, seven hundred sixty-eight million, two hundred eleven thousand, four hundred fifty-five");
     }
 
 }
